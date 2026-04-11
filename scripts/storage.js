dc6c64e3e46434f0922aef3001a102161dbbec4a
isabellaorear31-design <isabella.orear31@gmail.com>
2026-04-11 16:26:02 -0500

    Update storage.js

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,1 +1,17 @@
+export function savePlayerData(name) {
+    localStorage.setItem('horrorSurvivorName', name);
+}
 
+
+export function getPlayerData() {
+    return localStorage.getItem('horrorSurvivorName') || "";
+}
+
+
+export function saveDifficulty(level) {
+    localStorage.setItem('horrorDifficulty', level);
+}
+
+export function getDifficulty() {
+    return localStorage.getItem('horrorDifficulty') || "Easy";
+}
